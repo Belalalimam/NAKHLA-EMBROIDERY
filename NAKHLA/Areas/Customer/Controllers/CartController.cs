dfac9929f393ddca818f3523dd783930dd2d941a
Belalalimam <bb5503344@gmail.com>
2025-12-22 04:26:48 +0200

    update the navbar

--- NAKHLA/Areas/Customer/Controllers/CartController.cs
+++ NAKHLA/Areas/Customer/Controllers/CartController.cs
@@ -24,3 +24,4 @@ namespace NAKHLA.Areas.Customer.Controllers
 
 
     }
+}
--- NAKHLA/wwwroot/js/site.js
+++ NAKHLA/wwwroot/js/site.js
@@ -1,36 +1,68 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const menuTrigger = document.querySelector('.trigger');
+const closeTrigger = document.querySelector('.mini-close');
+const giveClass = document.querySelector('.site');
 
-// Write your JavaScript code.
-$('.close , .showmore').on('click', function () {
-    $('.card').toggleClass('flipped');
-    $('.close').toggleClass('visible');
+menuTrigger.addEventListener('click', function () {
+    giveClass.classList.add('showmenu');
 });
 
-//Gallery
-var galleryWidth = parseInt($('ul').css('width'), 10);
-var currentLeft = parseInt($('ul').css('left'), 10);
+closeTrigger.addEventListener('click', function () {
+    giveClass.classList.remove('showmenu');
+});
 
-// NEXT button
-$('#next').on('click', function (e) {
-    e.preventDefault();
-    if (Math.abs(currentLeft) < (galleryWidth - 300)) {
-        $('.gallery').css('left', (currentLeft - 300) + 'px');
-        currentLeft -= 300;
-    } else {
-        $('.gallery').css('left', 0 + 'px');
-        currentLeft = 0;
+
+
+//submenu
+const button = document.querySelectorAll('.has-child > a'),
+    modalheight = document.querySelector('.menu-list');
+
+button.forEach((item) => item.parentNode.classList.remove('expand'));
+button.forEach((menu) => menu.addEventListener('click', function () {
+    let modal = document.querySelector('.menu-list');
+    modal.classList.toggle('show');
+
+    if (this.parentNode.classList != 'expand') {
+        this.parentNode.classList.toggle('expand')
     }
-});
-
-// PREV button
-$('#prev').on('click', function (e) {
-    e.preventDefault();
-    if (Math.abs(currentLeft) >= (galleryWidth - 300)) {
-        $('.gallery').css('left', (currentLeft + 300) + 'px');
-        currentLeft += 300;
+    if (!modal.classList.contains('show')) {
+        modal.style.height = modalheight + 'px';
     } else {
-        $('.gallery').css('left', 0 + 'px');
-        currentLeft = -900;
+        modal.style.height = (this.parentNode.querySelector('ul').offsetHeight + 45) + 'px';
     }
-});
+
+    //back button
+
+    menu.parentNode.querySelector('.back').addEventListener('click', function () {
+        modal.style.height = 'auto';
+        modal.classList.remove('show');
+        menu.parentNode.classList.remove('expand')
+    })
+}))
+
+const topmenu = document.querySelectorAll('.top li a ');
+for (let i = 0; i < topmenu.length; i++) {
+    topmenu[i].addEventListener('click', function () {
+
+        let current = document.querySelectorAll('.active');
+
+        //remove active class
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+            document.querySelector(`#${this.classList}`).classList.remove('active')
+            return;
+        }
+
+        //if there's no active class
+        if (current.length > 0) {
+            current[0].classList.remove('active');
+            document.querySelector(`#${current[0].className}`).classList.remove('active');
+        }
+
+        //add active to id 
+        document.querySelector(`#${this.className}`).classList.add('active');
+
+        //add active class to the current/clicked button
+        this.classList.add('active');
+
+    })
+}
